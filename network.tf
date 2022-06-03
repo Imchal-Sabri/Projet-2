3b3ecf985c17d2be8adc8b716bc41535ae4c8f10
Imchal-Sabri <83274871+Imchal-Sabri@users.noreply.github.com>
2022-06-03 17:05:57 +0200

    Update network.tf

--- network.tf
+++ network.tf
@@ -28,3 +28,16 @@ resource "aws_route" "<##INFRA_NAME##>-defroute" {
         destination_cidr_block = "0.0.0.0/0"
         gateway_id = "${aws_internet_gateway.<##INFRA_NAME##>-igw.id}"
 }
+
+resource "aws_route_table" "<##INFRA_NAME##>-privrtb" {
+        vpc_id = aws_vpc.<##INFRA_NAME##>-vpc.id
+        route {
+                cidr_block     = "0.0.0.0/0"
+                nat_gateway_id = aws_nat_gateway.<##INFRA_NAME##>-natgw.id
+        }
+}
+
+resource "aws_route_table_association" "<##INFRA_NAME##>-privrtb-assoc1" {
+        route_table_id = aws_route_table.<##INFRA_NAME##>-privrtb.id
+        subnet_id      = aws_subnet.<##INFRA_NAME##>-priv.id
+}
